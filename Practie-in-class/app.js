a35f81bc372d0701bcbdeebaab90e9c495a45351
Muhammad Anas <151528713+anas152@users.noreply.github.com>
2024-09-13 09:17:23 +0500

    Add files via upload

---
+++ Practie-in-class/app.js
@@ -0,0 +1,39 @@
+// for (let i = 0; i < 3 ; i++){
+//     console.log("Yah itni " + i + " par chal ga");
+//     for (let j = 0; j < 3 ; j++)
+//     console.count("shezhad")
+// }
+
+
+// for ( let i = 0; i < 4; i++){
+//     for (let j = 0; i < 3; i++){
+//         console.log(i=$( ))
+//     }
+// }
+
+
+// let arr = [
+//     [10,45,2],
+//     [1,2,32],
+//     [74,5,9],
+// ]
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = 0; j < arr.length; j++){
+//         console.log(arr[i][j]) 
+//     }
+// }
+
+const arr = [
+    [12, 4, 3, 6],
+    [43 , 5],
+    [7],
+    [8, 9, 10],
+]
+
+for (let i = 0; i < arr.length; i ++){
+    for (let j = 0; j < arr[i].length; j ++){
+        console.log(arr[i][j])
+        
+    }
+}
