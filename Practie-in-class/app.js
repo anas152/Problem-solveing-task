7a38769727342e8d93902ac60863538ceca4d966
Muhammad Anas <anassoormo775@gmail.com>
2024-09-14 13:05:43 +0500

    'add'

--- Practie-in-class/app.js
+++ Practie-in-class/app.js
@@ -4,13 +4,11 @@
 //     console.count("shezhad")
 // }
 
-
 // for ( let i = 0; i < 4; i++){
 //     for (let j = 0; i < 3; i++){
 //         console.log(i=$( ))
 //     }
 // }
-
 
 // let arr = [
 //     [10,45,2],
@@ -20,20 +18,53 @@
 
 // for(let i = 0; i < arr.length; i++){
 //     for(let j = 0; j < arr.length; j++){
-//         console.log(arr[i][j]) 
+//         console.log(arr[i][j])
 //     }
 // }
 
-const arr = [
-    [12, 4, 3, 6],
-    [43 , 5],
-    [7],
-    [8, 9, 10],
-]
+// const arr = [
+//     [12, 4, 3, 6],
+//     [43 , 5],
+//     [7],
+//     [8, 9, 10],
+// ]
 
-for (let i = 0; i < arr.length; i ++){
-    for (let j = 0; j < arr[i].length; j ++){
-        console.log(arr[i][j])
-        
+// for (let i = 0; i < arr.length; i ++){
+//     for (let j = 0; j < arr[i].length; j ++){
+//         console.log(arr[i][j])
+
+//     }
+// }
+
+const fruits = ["apple", "oanana", "orange", "peach"];
+
+for (let i = 0; i < fruits.length; i++){
+
+    if( fruits[i] === "cake"){
+        isFound = "mil gya ha ";
     }
+
 }
+
+console.log(isFound);
+
+// const matrix1 = [
+//   [3, 3, 6],
+//   [32, 6, 85],
+//   [4, 7, 4],
+// ];
+
+// const matrix2 = [
+//   [4, 2, 5],
+//   [12, 6, 45],
+//   [5, 7, 3],
+// ];
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     console.log(matrix1[i][j] + matrix2[i][j]);
+//   }
+// }
+
+
+//Assignment We have 2 matrix array and we need get new nested array via excisting 1,2 metrix array.
