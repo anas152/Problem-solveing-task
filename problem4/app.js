0e74c2ce1f0e58fac462c3dc0e1d876da26edb70
Muhammad Anas <anassoormo775@gmail.com>
2024-09-21 13:03:30 +0500

    'tsting'

---
+++ problem4/app.js
@@ -0,0 +1,146 @@
+// for (let i = 0; i < 3 ; i++){
+//     console.log("Yah itni " + i + " par chal ga");
+//     for (let j = 0; j < 3 ; j++)
+//     console.count("shezhad")
+// }
+
+// for ( let i = 0; i < 4; i++){
+//     for (let j = 0; i < 3; i++){
+//         console.log(i=$( ))
+//     }
+// }
+
+// let arr = [
+//     [10,45,2],
+//     [1,2,32],
+//     [74,5,9],
+// ]
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = 0; j < arr.length; j++){
+//         console.log(arr[i][j])
+//     }
+// }
+
+// const arr = [
+//     [12, 4, 3, 6],
+//     [43 , 5],
+//     [7],
+//     [8, 9, 10],
+// ]
+
+// for (let i = 0; i < arr.length; i ++){
+//     for (let j = 0; j < arr[i].length; j ++){
+//         console.log(arr[i][j])
+
+//     }
+// }
+
+// /const fruit = ["apple", "banana", "peach", "mango"]; // Corrected spelling
+// let found = false;
+
+// for (let i = 0; i < fruit.length; i++) {
+//     if (fruit[i].toLowerCase() === "banana") { 
+//         console.log(`Found: ${fruit[i]}`);
+//         found = true;
+//         break; 
+//     }
+// }
+
+// if (!found) {
+//     console.log("Not found");
+// }
+
+
+// const fruit = ["Apple", "Banana", "Peach", " mango"];
+// let found = false;
+// for (let i = 0 ; i < fruit.length; i++){
+//     if(fruit[i].toLowerCase() === "apple"){
+//         console.log(`Found : ${fruit[i]}`); 
+//         found = true;
+//         break;
+
+//     }
+// }
+// if(!found){
+//     console.log("Not found");
+//     }
+// const matrix1 = [
+//   [3, 3, 6],
+//   [32, 6, 85],
+//   [4, 7, 4],
+// ];
+
+// const matrix2 = [
+//   [4, 2, 5],
+//   [12, 6, 45],
+//   [5, 7, 3],
+// ];
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     console.log(matrix1[i][j] + matrix2[i][j]);
+//   }
+// }
+
+
+//Assignment We have 2 matrix array and we need get new nested array via excisting 1,2 metrix array.
+
+// const matrix1 = [
+//   [3, 1, 6],
+//   [32, 6, 85],
+//   [4, 7, 4],
+// ];
+
+// const matrix2 = [
+//   [4, 2, 5],
+//   [12, 6, 45],
+//   [5, 7, 3],
+// ];
+
+// const result = [[]];
+
+// for (let i = 0; i < 3; i++){
+//     let row = [];
+//     for (let j = 0; i < 3; i++){
+//         row.push(matrix1[i][j] + matrix2[i][j]);
+//         result.push(row)
+//     }
+// }
+// console.log(result)
+
+
+// const matrix1 = [
+//     [21, 1, 65,],
+//     [1, 35, 1],
+//     [4, 53, 21]
+// ]
+// const matrix2 = [
+//   [4, 2, 5],
+//   [12, 6, 45],
+//   [5, 7, 3],
+// ];
+
+// const resultMatrix = [];
+// for(let i = 0; i < 3; i++){
+//     let row = [];
+//     for (let j = 0; j < 3; j++){
+//         row.push(matrix1[i][j] + matrix2[i][j]);
+
+//     }
+//     resultMatrix.push(row);
+// }
+
+
+// console.log(resultMatrix);
+
+
+
+//is ma kabhi kabhi masla aata ha like negative ki value ma toh hum aik formula run karta ha ((a,b) => a-b)
+
+// let num = [1,3,5,0,6,-100,-34];
+// num.sort()
+// console.log(num);
+
+//Assignemnt is Get average try to get with 2 desimals and leave others "average" we have 10 values + all vlaues and divide buy number of valuse E.g  "1234 = value" forlmula total number divdied buy total number valuse. via using Funciton ;
+
